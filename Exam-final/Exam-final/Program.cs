8fc8f6c54ba46ce727ff72fab09df6af9c9491d2
Calvin Bourdet <2335296@etu.cegepjonquiere.ca>
2024-05-18 23:01:35 -0400

    vs

---
+++ Exam-final/Exam-final/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Exam_final
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
